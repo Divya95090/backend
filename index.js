d12c43aeec886c35fa69e6b8d9c8a36852d66e99
versing012 <carbene93145@gmail.com>
2025-04-16 08:36:50 +0530

    added a github api

--- index.js
+++ index.js
@@ -6,6 +6,41 @@ const app = express()  //using express to make a variable app
 
 const port = 3000 //this is basically a virtual port where the app is listening
 
+const githubData = {
+  "login": "Divya95090",
+  "id": 112494726,
+  "node_id": "U_kgDOBrSIhg",
+  "avatar_url": "https://avatars.githubusercontent.com/u/112494726?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/Divya95090",
+  "html_url": "https://github.com/Divya95090",
+  "followers_url": "https://api.github.com/users/Divya95090/followers",
+  "following_url": "https://api.github.com/users/Divya95090/following{/other_user}",
+  "gists_url": "https://api.github.com/users/Divya95090/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/Divya95090/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/Divya95090/subscriptions",
+  "organizations_url": "https://api.github.com/users/Divya95090/orgs",
+  "repos_url": "https://api.github.com/users/Divya95090/repos",
+  "events_url": "https://api.github.com/users/Divya95090/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/Divya95090/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": "Divya Khandelwal",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 9,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 1,
+  "created_at": "2022-08-30T14:15:37Z",
+  "updated_at": "2025-04-10T07:50:19Z"
+}
 //we are getting the app to listen at '/' which is home route 
 //means if any response comes at '/' route we are going to send a response "Hello World"
 app.get('/', (req, res) => {
@@ -22,6 +57,10 @@ app.get('/login',(req,res)=>{
 
 app.get('/youtube',(req,res)=>{
     res.send('Chai aur Code')
+})
+
+app.get('/github',(req,res)=>{
+  res.json(githubData)
 })
 
 //we are now telling app to listen to port 
